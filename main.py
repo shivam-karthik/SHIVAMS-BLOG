17eb8fc712f75a0d1ecdae6fb12c2e302ab4a465
shivam karthik <86833590+shivam-karthik@users.noreply.github.com>
2022-02-12 23:27:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ def add_new_post():
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @admin_only
 def edit_post(post_id):
-    post = BlogPost.query.get(post_id)
+    post = BlogPost.query.get()
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
